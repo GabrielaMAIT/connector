017619fb580613eaf4a98704c2145c6e5a6cbafc
gabi <gabriela@mait.pl>
2024-06-03 19:42:07 +0200

    initial commit: comment

--- src/main/java/mait/springframework/converters/ProductToProductForm.java
+++ src/main/java/mait/springframework/converters/ProductToProductForm.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 public class ProductToProductForm implements Converter<Product, ProductForm> {
     @Override
     public ProductForm convert(Product product) {
+        //typical product desc
         ProductForm productForm = new ProductForm();
         productForm.setId(product.getId());
         productForm.setDescription(product.getDescription());
